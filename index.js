69fd63e8d63d661a8e0ed4fc78b8f5e136e40bd3
Ambi <ptlc.ptlc.ptlc@gmail.com>
2025-10-25 22:38:05 +0200

    frenquency + audit fix

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const Discord = require("discord.js");
 require("dotenv").config();
 
-const client = new Discord.Client({ intents:
-	[
+const reactFrequency = parseFloat(process.env.REACT_FREQUENCY) || 1;
+
+const client = new Discord.Client({
+	intents: [
 		Discord.GatewayIntentBits.Guilds,
 		Discord.GatewayIntentBits.GuildMessages,
 		Discord.GatewayIntentBits.GuildEmojisAndStickers
@@ -10,17 +12,20 @@ const client = new Discord.Client({ intents:
 });
 
 client.on("ready", () => {
-	console.info("Logged in Discord as "+client.user.tag);
+	console.info("Logged in Discord as " + client.user.tag);
 });
 
 client.on("messageCreate", async message => {
-	var emojis = [];
-	message.guild.emojis.cache.forEach(emoji => {
-		if (emoji.name.startsWith("AAAAA"))
-			emojis.push(emoji);
-	});
-	var emoji = emojis.length ? emojis[Math.floor(emojis.length*Math.random())] : "👻";
-	message.react(emoji);
+	if (message.author.id == client.user.id) return;
+	if (Math.random() < reactFrequency) {
+		var emojis = [];
+		message.guild.emojis.cache.forEach(emoji => {
+			if (emoji.name.startsWith("AAAAA"))
+				emojis.push(emoji);
+		});
+		var emoji = emojis.length ? emojis[Math.floor(emojis.length*Math.random())] : "👻";
+		message.react(emoji);
+	}
 });
 
 client.login(process.env.DISCORD_BOT_TOKEN);
